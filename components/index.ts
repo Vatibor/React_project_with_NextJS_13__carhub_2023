887c157272ad8f80d8d4150f08ee0af906305b86
Vatibor <vatibor2@gmail.com>
2023-06-30 14:27:08 +0200

    # Make Landpage section # Make Header and footer to separate components

---
+++ components/index.ts
@@ -0,0 +1,12 @@
+import Hero from './Hero'
+import Custombutton from './CustomButton'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+
+export {
+  Hero,
+  Custombutton,
+  Navbar,
+  Footer
+}
