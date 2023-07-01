8bd2770bb334e989ac173540ff74646b39919100
Vatibor <vatibor2@gmail.com>
2023-07-01 20:57:56 +0200

    # Finish server side rendering catalog section

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,16 @@
-import {CarProps} from "@/types";
+import {CarProps, FilterProps} from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters
+
   const headers = {
     'X-RapidAPI-Key': '5183f006a6mshd7085b979c3083cp1eb5e6jsneebe0d505237',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
 
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {headers: headers})
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}
+    &model=${model}&limit=${limit}&fuel_type=${fuel}`, {headers: headers})
 
   const result = await response.json()
 
